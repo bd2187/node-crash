336552f0ca8568baacd746c4e7409d5b941d3090
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2019-04-01 18:52:35 -0700

    Require and call fsDemo from ./fs-demo

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const referPath = require("./refer-path");
+const fsDemo = require("./fs-demo");
 const Person = require("./person");
 
 const brandon = new Person("Brandon", 25);
@@ -6,3 +7,4 @@ const brandon = new Person("Brandon", 25);
 brandon.greeting();
 
 referPath();
+fsDemo();
