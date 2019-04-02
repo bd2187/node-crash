790c058e646681f6651cbd066cd3744429942319
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2019-04-01 19:12:10 -0700

    Require and call osDemo

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const referPath = require("./refer-path");
 const fsDemo = require("./fs-demo");
+const osDemo = require("./os-demo");
 const Person = require("./person");
 
-const brandon = new Person("Brandon", 25);
+// const brandon = new Person("Brandon", 25);
 
-brandon.greeting();
+// brandon.greeting();
 
-referPath();
-fsDemo();
+// referPath();
+// fsDemo();
+osDemo();
