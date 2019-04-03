1134b0a64f1ebcd067445dcd972175175d468e69
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2019-04-02 18:50:51 -0700

    Acces myUrl properties. Log values to console

--- url-demo.js
+++ url-demo.js
@@ -2,7 +2,7 @@ const url = require("url");
 
 module.exports = function() {
     const myUrl = new URL(
-        "http://mywebsite.com/hello.html?id=100&status=active"
+        "http://mywebsite.com:8000/hello.html?id=100&status=active"
     );
 
     // Legacy:
@@ -10,4 +10,28 @@ module.exports = function() {
     //     "http://mywebsite.com/hello.html?id=100&status=active"
     // );
     // console.log(myLegacyUrl);
+
+    // Host (root domain)
+    console.log(`Host: ${myUrl.host}`);
+
+    // Hostname (excludes port if included)
+    console.log(`Hostname: ${myUrl.hostname}`);
+
+    // Pathname
+    console.log(`Pathname: ${myUrl.pathname}`);
+
+    // Serialized query
+    console.log(`Serialized query: ${myUrl.search}`);
+
+    // Params object
+    console.log(`Params object: ${myUrl.searchParams}`);
+
+    // Append param
+    myUrl.searchParams.append("channel", "live");
+    console.log(`Appended param: ${myUrl.href}`);
+
+    // Loop through params
+    myUrl.searchParams.forEach(function(value, name) {
+        console.log(`${name}: ${value}`);
+    });
 };
