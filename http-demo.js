74259d76b0a087049d94c68b8f2d74ecc0f9ef27
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2019-04-03 21:15:41 -0700

    Add routes to read files from public directory

--- http-demo.js
+++ http-demo.js
@@ -1,14 +1,35 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
 module.exports = function() {
     const PORT = process.env.PORT || 5000;
-
     http.createServer((req, res) => {
         if (req.url === "/") {
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end("<h1>homepage</h1>");
+            fs.readFile(
+                path.join(__dirname, "public", "index.html"),
+                (err, content) => {
+                    if (err) throw err;
+                    res.writeHead(200, { "Content-Type": "text/html" });
+                    res.end(content);
+                }
+            );
         }
-        res.end();
-    }).listen(PORT, () => {
-        console.log(`Now listening to port ${PORT}`);
-    });
+
+        if (req.url === "/about") {
+            fs.readFile(
+                path.join(__dirname, "public", "about.html"),
+                (err, content) => {
+                    if (err) throw err;
+                    res.writeHead(200, { "Content-Type": "text/html" });
+                    res.end(content);
+                }
+            );
+        }
+
+        if (req.url === "/api/users") {
+            res.writeHead(200, { "Content-type": "application/json" });
+            res.end(JSON.stringify({ user: "brandon", age: 10 }));
+        }
+    }).listen(PORT, () => console.log(`Now listening to port ${PORT}`));
 };
