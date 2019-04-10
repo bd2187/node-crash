b0943be2f7f5024225adf5126cf2ef84071e9fe8
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2019-04-09 18:32:21 -0700

    import and call httpDemo

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const referPath = require("./refer-path");
 const fsDemo = require("./fs-demo");
 const osDemo = require("./os-demo");
+const urlDemo = require("./url-demo");
+const eventDemo = require("./events-demo");
+const httpDemo = require("./http-demo");
+const Logger = require("./logger");
 const Person = require("./person");
 
 // const brandon = new Person("Brandon", 25);
@@ -9,4 +13,19 @@ const Person = require("./person");
 
 // referPath();
 // fsDemo();
-osDemo();
+// osDemo();
+
+// urlDemo();
+// eventDemo();
+
+/*
+var logger = new Logger();
+
+logger.on("message", data => {
+    console.log(data);
+});
+
+logger.log("hello world ");
+*/
+
+httpDemo();
